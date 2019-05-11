fe7b7b7c1996f7f961ebaff57be2cf50d454462e
NickWu <wujunao19950715@gmail.com>
2019-05-11 22:23:46 +0100

    Add sign in page with dummy components.
    Will connect form control and styling.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { MatCardModule, MatButtonModule, MatTabsModule, MatToolbarModule } from '@angular/material';
+import { MatCardModule, MatButtonModule, MatFormFieldModule, MatIconModule, MatInputModule, MatTabsModule, MatToolbarModule } from '@angular/material';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { StartComponent } from './start/start.component';
+import { SignInComponent } from './sign-in/sign-in.component';
 
 @NgModule({
-  declarations: [AppComponent, WelcomeComponent, StartComponent],
+  declarations: [AppComponent, WelcomeComponent, StartComponent, SignInComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatCardModule,
     MatButtonModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
     MatTabsModule,
     MatToolbarModule
   ],
